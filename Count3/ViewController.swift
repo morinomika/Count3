35012cd0dcc6b1620a4bf8554bb9c57341fb566d
Mika <iluvkatyperry1126@gmail.com>
2018-09-12 10:54:15 +0900

    Initial Commit

---
+++ Count3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Count3
+//
+//  Created by Mika on 2018-09-12.
+//  Copyright © 2018 Mika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
